7750020c28a447cc97124955cfd60640ec5e2964
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-01-27 17:59:49 -0600

    completed 2 problems

--- Sample.java
+++ Sample.java
@@ -1,7 +1,101 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+/******************************PROBLEM-1*******************/
+// Time Complexity : 0(n)
+// Space Complexity : 0(h)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> result=new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return new ArrayList<>();
+        }
+
+        dfs(root,0,new ArrayList<>(),targetSum);
+        return result;
+    }
+
+    private void dfs(TreeNode root,int sum,List<Integer> path,int targetSum){
+        if(root==null){
+            return;
+        }
+
+        sum+=root.val;
+        path.add(root.val);
+        if(root.left==null && root.right==null){
+            if(sum==targetSum){
+                result.add(new ArrayList<>(path));
+            }
+        }
+        dfs(root.left,sum,path,targetSum);
+        dfs(root.right,sum,path,targetSum);
+        path.remove(path.size()-1);
+    }
+}
+/******************************PROBLEM-2*******************/
+// Time Complexity : 0(N)
+// Space Complexity : 0(H)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isSymmetric=true;
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return false;
+        }
+        dfs(root.left,root.right);
+
+        return isSymmetric;
+
+    }
+
+    private void dfs(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return;
+        }
+        if(left==null || right==null){
+            isSymmetric=false;
+            return;
+        }
+        if(left.val != right.val){
+            isSymmetric=false;
+            return;
+        }
+        dfs(left.left,right.right);
+        dfs(left.right,right.left);
+    }
+}
