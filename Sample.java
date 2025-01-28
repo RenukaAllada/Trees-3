660bc03c6e3cde7798ff463b4a4522942b1a929d
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-01-27 18:21:00 -0600

    add extra solution

--- Sample.java
+++ Sample.java
@@ -99,3 +99,51 @@ class Solution {
         dfs(left.right,right.left);
     }
 }
+
+// Time Complexity : 0(N)
+// Space Complexity : 0(N)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        boolean isSymmetric = true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while (!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                isSymmetric = false;
+                break;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+
+        }
+        return isSymmetric;
+    }
+}
